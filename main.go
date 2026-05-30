4f79cdc866a5ed7a1f1204b5102a7ae7b4339c11
irpanzy <irfanoputra27@gmail.com>
2026-05-30 14:59:46 +0700

    feat: update main.go to declare variables and print user information

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
-	println("Hello, World!")
+	// Deklarasi variabel dengan tipe data eksplisit
+	var name string = "Irpanzy"
+	var age int = 22
+	// Deklarasi variabel dengan tipe data implisit (type inference)
+	city := "Yogyakarta"
+	year := 2026
+	fmt.Println("Nama:", name)
+	fmt.Println("Age:", age)
+	fmt.Println("City:", city)
+	fmt.Println("Year:", year)
 }
