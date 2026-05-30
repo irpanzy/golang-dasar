f3d43b427fed126a30cd8ce0f3b0c10126d6cb70
irpanzy <irfanoputra27@gmail.com>
2026-05-30 15:04:06 +0700

    feat: enhance main.go with additional variable declarations and print statements

--- main.go
+++ main.go
@@ -6,11 +6,21 @@ func main() {
 	// Deklarasi variabel dengan tipe data eksplisit
 	var name string = "Irpanzy"
 	var age int = 22
+
 	// Deklarasi variabel dengan tipe data implisit (type inference)
 	city := "Yogyakarta"
 	year := 2026
+
+	// Deklarasi konstanta
+	const pi = 3.14
+	const appName = "Learning Go"
+
 	fmt.Println("Nama:", name)
 	fmt.Println("Age:", age)
 	fmt.Println("City:", city)
 	fmt.Println("Year:", year)
+	fmt.Println("Pi:", pi)
+	fmt.Println("App Name:", appName)
+
+	pi = 3.14159 // Error: cannot assign to pi (constant)
 }
